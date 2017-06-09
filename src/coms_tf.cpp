676db2da673668fa0e64d459b74ceaa481d0befc
wolfram2012 <qiyuan.liu@tusimple.com>
2017-06-09 17:08:02 +0900

    show trajectory

--- src/coms_tf.cpp
+++ src/coms_tf.cpp
@@ -1,18 +1,34 @@
 #include "ros/ros.h"
+#include "tf/tf.h"
 #include "tf/transform_broadcaster.h"
 #include "geometry_msgs/PoseStamped.h"
 
+double pose_x = 0;
+double pose_y = 0;
+
+double quat_x = 0;
+double quat_y = 0;
+double quat_z = 0;
+double quat_w = 1;
 
 void poseCallback(const geometry_msgs::PoseStamped& point)
 {
+    pose_x = point.pose.position.x;
+    pose_y = point.pose.position.y;
+    //ROS_INFO("I heard somthing");
+    quat_x = point.pose.orientation.x;
+    quat_y = point.pose.orientation.y;
+    quat_z = point.pose.orientation.z;
+    quat_w = point.pose.orientation.w;
+    //ROS_INFO("pose_x: %f; pose_y: %f.",pose_x, pose_y);
+}
+
+void poseInit()
+{
     static tf::TransformBroadcaster broadcaster;
     tf::Transform transform;
-    transform.setOrigin(tf::Vector3(point.pose.position.x, point.pose.position.y, 0) );
-    tf::Quaternion q(
-    	point.pose.orientation.x,
-    	point.pose.orientation.y,
-    	point.pose.orientation.z,
-    	point.pose.orientation.w );
+    transform.setOrigin(tf::Vector3(1, 1, 0) );
+    tf::Quaternion q(0, 0, 0, 1);
 
     transform.setRotation(q);
     broadcaster.sendTransform(
@@ -24,9 +40,25 @@ int main(int argc, char** argv)
 {
     ros::init(argc, argv, "coms_tf_broadcaster");
     ros::NodeHandle n;
+    tf::TransformBroadcaster broadcaster;
+    ros::Subscriber tf_sub = n.subscribe("/base_pose",100, poseCallback);
+    ros::Rate r(100);
+    
+    double th = 0;
+    while(n.ok())
+    {
+	th += 0.25;
+	broadcaster.sendTransform(
+         tf::StampedTransform(
+           tf::Transform( tf::Quaternion(quat_x, quat_y, quat_z, quat_w), tf::Vector3(pose_x, pose_y, 0)),
+           ros::Time::now(),"map", "coms"));
+	ROS_INFO("while-> pose_x: %f; pose_y: %f.",pose_x, pose_y);
+        r.sleep();
+	ros::spinOnce();
+    }
+//    poseInit();
+    
 
-    ros::Subscriber tf_sub = n.subscribe("base_pose",10,&poseCallback);
-
-    ros::spin();
+    
     return 0;
 }
--- src/coms_trajectory.cpp
+++ src/coms_trajectory.cpp
@@ -6,6 +6,8 @@
 #include <tf/transform_listener.h>
 #include "joystick2coms/VehicleMessage.h"
 
+#include "visualization_msgs/Marker.h"
+
 double steering = 0.0;
 double speed = 0.0;
 
@@ -19,6 +21,9 @@ double pi = 3.141593;
 ros::Publisher pose_pub;
 ros::Publisher tra_pub;
 nav_msgs::Path trajectory;
+ros::Publisher marker_pub;
+
+visualization_msgs::Marker marker;
 
 void vehicleMessage(const joystick2coms::VehicleMessage& msg)
 {
@@ -28,15 +33,15 @@ void vehicleMessage(const joystick2coms::VehicleMessage& msg)
 
 void transformPoint(const tf::TransformListener &listener)
 {
-    dis = speed * 0.1;	
+    dis = speed * 0.05;	
     kk  = asin((dis * tan(steering)/3.06));
     delta_x  = dis*sin(kk);
     delta_y  = dis*cos(kk);;
-    delta_th = 2 * kk;
+    delta_th = 2*kk;
 
     geometry_msgs::PoseStamped vb_point;
     vb_point.header.stamp = ros::Time();
-    vb_point.header.frame_id = "map";
+    vb_point.header.frame_id = "coms";
     vb_point.pose.position.x = delta_x;
     vb_point.pose.position.y = delta_y;
     vb_point.pose.position.z = 0;
@@ -56,6 +61,10 @@ void transformPoint(const tf::TransformListener &listener)
 	trajectory.header.frame_id = "map";
         trajectory.poses.push_back(global_point);
         tra_pub.publish(trajectory);
+
+	marker.pose.position.x = global_point.pose.position.x;
+	marker.pose.position.y = global_point.pose.position.y;
+	marker_pub.publish(marker);
     }
     catch(tf::TransformException& ex)
     {
@@ -69,9 +78,32 @@ int main(int argc, char** argv)
 {
     ros::init(argc, argv, "trajectory");
     ros::NodeHandle n;
-    pose_pub = n.advertise<geometry_msgs::PoseStamped>("base_pose",1);
+    pose_pub = n.advertise<geometry_msgs::PoseStamped>("base_pose",100);
     tra_pub = n.advertise<nav_msgs::Path>("trajectory",1);
     ros::Subscriber sub = n.subscribe("/coms/vehiclemessage",1000,vehicleMessage);
+
+    marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 10);
+
+marker.header.frame_id = "coms";
+marker.header.stamp = ros::Time::now();
+marker.ns = "basic_shapes";
+marker.id = 0;
+marker.type = visualization_msgs::Marker::CUBE;
+marker.action = visualization_msgs::Marker::ADD;
+marker.pose.position.x = 0;
+marker.pose.position.y = 0;
+marker.pose.position.z = 0;
+marker.pose.orientation.x = 0.0;
+marker.pose.orientation.y = 0.0;
+marker.pose.orientation.z = 0.0;
+marker.pose.orientation.w = 1.0;
+marker.scale.x = 0.1;
+marker.scale.y = 0.1;
+marker.scale.z = 0.1;
+marker.color.r = 1.0f;
+marker.color.g = 1.0f;
+marker.color.b = 0.0f;
+marker.color.a = 1.0;
 
     tf::TransformListener listener(ros::Duration(0.05));  
     //we'll transform a point once every second
